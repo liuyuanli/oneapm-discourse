ae307b1b8f816dace55d28c91bf15bc090068326
Jerry Liu <send2me@qq.com>
2016-05-21 10:58:26 +0800

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: OneAPM RPM
+# about: Report performance data to http://www.oneapm.com/
+# version: 0.0.1
+# authors: Sam Saffron, David Celis, Jerry Liu
+
+ENV['BWCONFIG'] = File.expand_path('../oneapm.yml', __FILE__)
+gem 'oneapm_rpm'
+
+OneAPM::Control.instance.init_plugin config: Rails.configuration
+
+after_initialize do
+  ForumsController.oneapm_ignore only: [:status]
+end
