8cb8e24206713fdc03355c0aeb53838092c2e736
Jerry Liu <send2me@qq.com>
2016-05-21 10:58:53 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: OneAPM RPM
 # about: Report performance data to http://www.oneapm.com/
 # version: 0.0.1
-# authors: Sam Saffron, David Celis, Jerry Liu
+# authors: Sam Saffron, David Celis
 
 ENV['BWCONFIG'] = File.expand_path('../oneapm.yml', __FILE__)
 gem 'oneapm_rpm'
